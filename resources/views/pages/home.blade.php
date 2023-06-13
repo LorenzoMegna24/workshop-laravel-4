df07d6e113d8a16923ebad36f5b8031429612304
SimoneScordato99 <scordato.simone@yahoo.com>
2023-06-13 17:18:39 +0200

    add: foreach

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,4 +1,11 @@
 @extends('welcome')
 @section('pizza')
-    
+    @foreach($pizzas as $elem)
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">{{$elem['nome_pizza']}}</h5>
+            <p class="card-text">{{$elem['ingredienti']}}</p>
+        </div>
+    </div>
+    @endforeach
 @endsection
