7621da5b62c951706d9a405a0f64d8cf5a7c0f1b
LorenzoMegna24 <megnalorenzo0@gmial.com>
2023-06-13 16:12:35 +0200

    Creazione tabella

--- database/migrations/2023_06_13_140113_create_pizzas_table.php
+++ database/migrations/2023_06_13_140113_create_pizzas_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('pizzas', function (Blueprint $table) {
             $table->id();
+            $table->string('nome_pizza');
+            $table->string('ingredienti');
+            $table->string('impasto');
+            $table->dateTime('tempo_preparazione');
+            $table->boolean('disponibilità')->default(true);
             $table->timestamps();
         });
     }
