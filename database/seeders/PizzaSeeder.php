ca0af766bc27de5516593c8679338e1a437f7ffb
SimoneScordato99 <scordato.simone@yahoo.com>
2023-06-13 17:14:23 +0200

    add: seeder

--- database/seeders/PizzaSeeder.php
+++ database/seeders/PizzaSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Pizza;
+use Faker\Generator as Faker;
+
 
 class PizzaSeeder extends Seeder
 {
@@ -12,8 +15,19 @@ class PizzaSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        
+        for($i=0; $i < 20; $i++){
+            $newPizza = new Pizza();
+            $newPizza->nome_pizza =$faker->firstNameFemale() ;
+            $newPizza->ingredienti = 'pomodoro '.'mozzarella '.$faker->randomElement(['prosciutto', 'salame', 'olive', 'funghi']);
+            $newPizza->impasto = $faker->randomElement(['00', 'integrale', 'tumminia', 'kamut']);  
+            $newPizza->tempo_preparazione = $faker->dateTimeBetween('30s','120s');
+            $newPizza->disponibilità = $faker->randomElement([true, false]);
+            $newPizza->created_at = $faker->time();
+            $newPizza->updated_at = $faker->time();
+            $newPizza->save();
+        }
     }
 }
